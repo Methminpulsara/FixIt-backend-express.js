2c9288e506dfdfe6029a063dd12b2716376ed751
Methminpulsara <methminpulsara10@gmail.com>
2025-12-15 23:53:39 +0530

    update

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -13,18 +13,6 @@
 
 //     const token = authHeader.split(" ")[1];
 
-//     try {
-        
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-//         req.user = decoded;
-//         next()
-
-//     } catch (error) {
-//         res.status(400),json({message: "Invalid token - authMiddleware"})
-//     }
-
-
-// }
 
 
 const jwt = require("jsonwebtoken");
