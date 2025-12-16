ef0fe9b9664f8cfb36b8f5e3d0edc65aca886ffb
Methminpulsara <methminpulsara10@gmail.com>
2025-12-16 22:09:33 +0530

    add request Schema

--- src/models/Garage.js
+++ src/models/Garage.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose')
+
 const GarageSchema = new mongoose.Schema({
+
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
     address: String,
-    services: [String], // e.g., ["Tire Repair", "Battery Change"]
+    // services: [String],["Tire Repair", "Battery Change"]
     photos: [String],
+
     verificationStatus: {
         type: String,
         enum: ["pending", "approved", "rejected"],
         default: "pending"
     },
+
     isVerified: {
         type: Boolean,
         default: false
     },
+
     isAvailable: {
         type: Boolean,
-        default: true // මුලින්ම Job එකක නිරත නොවන බව සලකයි.
+        default: true 
     },
+
 }, { timestamps: true });
-// Location index එක User Schema එකේ තිබෙන නිසා මෙහි අනවශ්‍යයි.
+
+
 module.exports = mongoose.model("Garage", GarageSchema);
---
+++ src/models/Request.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+
+const RequestSchema = new mongoose.Schema({
+
+
+    // request ek dhmme kud = customer 
+    customerId :{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+
+    // job ek accept krnne kud 
+    providerId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+
+    // Request type
+    requetsType:{
+        type:String, 
+        enum:["mechanic", "garage"],
+        required:true
+    },
+
+    // issue and vehicle details 
+    issueDescription: {type:String, required:true },
+    vehicleDetails: {type:String},
+
+    // request location
+    location:{
+        type: {type:String, enum:["Point"], default:"Point"},
+        coordinates: {type:[Number] ,required:true}// longitude, latitude
+    },
+
+    // job eke accep or reject
+    status:{
+        type:String,
+        enum:["pending", "accepted", "in_progress" , "completed", "cancelled"],
+        default:"pending"
+    },
+
+    // time
+    acceptedAt:{type:Date,default:null},
+    completedAt:{type:Date, default:null}
+
+},{timestamps:true});
+
+//  Geo-Spatial Queries For Index 
+RequestSchema.index({ location: "2dsphere" });
+
+module.exports = mongoose.model("Request" , RequestSchema)
