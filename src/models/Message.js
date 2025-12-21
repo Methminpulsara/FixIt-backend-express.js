04e687957a2ac0584a7f18c5cdc9a601e1026fa3
Methminpulsara <methminpulsara10@gmail.com>
2025-12-21 15:19:09 +0530

    create message Schema

---
+++ src/models/Message.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const MessageSchema = new mongoose.Schema({
+
+    requestId:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Request",
+        required : true
+    },
+    senderId:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    receivedId:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    message:{
+        type : String,
+        required : true
+    },
+    isReadL:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps: true});
+
+module.exports = mongoose.model("Message", MessageSchema)
