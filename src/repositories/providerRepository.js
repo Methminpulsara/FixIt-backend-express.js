457988ae01bde6e23449b2d7b25cfe248d90e957
Methminpulsara <methminpulsara10@gmail.com>
2025-12-21 13:27:31 +0530

    add providers repository

--- src/repositories/providerRepository.js
+++ src/repositories/providerRepository.js
@@ -3,7 +3,6 @@ exports.findNearProviders = async (lng, lat, maxDistance, type) => {
     return User.find({
         type: type, // 'mechanic' හෝ 'garage' ලෙස dynamic ලෙස ලබා දෙයි
         isVerified: true,
-        isAvailable:true,
         // මෙහිදී 'isAvailable' field එකක් තිබේ නම් එයත් check කරන්න
         location: {
             $near: {
