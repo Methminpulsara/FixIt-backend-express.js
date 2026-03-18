7f92b09e27db20c28fa001356a0cab935ee844b7
Methminpulsara <methminpulsara10@gmail.com>
2026-03-18 14:08:48 +0530

    update message schema

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,33 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const MessageSchema = new mongoose.Schema({
-
-    requestId:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Request",
-        required : true
+const MessageSchema = new mongoose.Schema(
+  {
+    requestId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Request',
+      required: true,
     },
-    senderId:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
-    receiverId:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
-  messageType: { 
-    type: String, 
-    enum: ['text', 'image'], 
-    default: 'text' 
-},
-    isRead:{
-        type:Boolean,
-        default:false
-    }
-},{timestamps: true});
+    message: {
+      type: String,
+      trim: true,
+      required: function requiredMessage() {
+        return this.messageType === 'text';
+      },
+    },
+    messageType: {
+      type: String,
+      enum: ['text', 'image'],
+      default: 'text',
+    },
+    imageUrl: {
+      type: String,
+      default: null,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-MessageSchema.index({ requestId: 1, createdAt: 1 }); // මේකෙන් Query එක ගොඩක් වේගවත් වෙනවා
+MessageSchema.index({ requestId: 1, createdAt: 1 });
 
-module.exports = mongoose.model("Message", MessageSchema)
+module.exports = mongoose.model('Message', MessageSchema);
