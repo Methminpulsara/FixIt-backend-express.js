bac15a08bd6be02465af3c8dca6b12dee3ce19bf
Methminpulsara <methminpulsara10@gmail.com>
2026-03-30 15:48:15 +0530

    update review routes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const authMiddleware = require('../middleware/authMiddleware')
-const reviewController = require('../controllers/reviewController')
+const express = require('express');
+const router = express.Router();
 
+const authMiddleware = require('../middleware/authMiddleware');
+const reviewController = require('../controllers/reviewController');
 
-router.post('/', authMiddleware , reviewController.createReview)
+router.get('/provider/:providerId', reviewController.getProviderReviews);
+router.get('/provider/:providerId/summary', reviewController.getProviderReviewSummary);
 
-router.put('/:id', authMiddleware, reviewController.updateReview); 
-
+router.get('/request/:requestId/mine', authMiddleware, reviewController.getMyReviewByRequest);
+router.post('/', authMiddleware, reviewController.createReview);
+router.put('/:id', authMiddleware, reviewController.updateReview);
 router.delete('/:id', authMiddleware, reviewController.deleteReview);
 
-
-module.exports =  router;
+module.exports = router;
